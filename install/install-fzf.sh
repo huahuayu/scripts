8680a1143dad6dbf4ab3ea3abf4f78ae0ac29e6a
shiming <codingmylife@gmail.com>
2022-07-25 00:35:55 +0800

    add fzf

---
+++ install/install-fzf.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+# tested in
+# - [] macos
+# - [x] ubuntu
+# - [] centos
+# - [] apline
+# - [] archlinux
+
+has() {
+    hash "$1" 2>/dev/null
+    return $?
+}
+
+if !(has git); then
+    bash $(cd $(dirname $0) && pwd)/install-git.sh
+fi
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
