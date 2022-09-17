3076e3d14d927909d9e8211b0da3d962338f981e
shiming <codingmylife@gmail.com>
2022-09-17 23:34:56 +0800

    explicit specify user name

--- docker/postgres.sh
+++ docker/postgres.sh
@@ -1,1 +1,1 @@
-docker run -d --name postgres -e POSTGRES_PASSWORD=root -p 5432:5432 -v /data/postgres:/var/lib/postgresql/data postgres
+docker run -d --name postgres --restart always -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root -p 5432:5432 -v /data/postgres:/var/lib/postgresql/data postgres
